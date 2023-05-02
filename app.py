a2b9ab900e8dd75e953f1fde1557c8abf0d3d0b4
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2023-05-02 17:15:11 -0500

    the game functions

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from operator import contains
 from flask import Flask, render_template, request
 
+import random
+
+
 app = Flask(__name__)
-
 high_score = 0  # Initialize the high score to 0
-
+score = 0
 
 import openai
 
@@ -15,30 +17,30 @@ openai.api_key = OPEN_API_KEY.mykey
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global high_score  # Use the global variable
-    score = 0
+    global score
     message = ''
 
     if request.method == 'POST':
-        question = request.form['question']
         user_answer = request.form['user_answer']
-
-        question = "How do we solve the isreal palestine conflict?"
-
+        question = request.form['question']
 
 
         if decide(question, user_answer):
             message = 'Correct!'
-            score = 1
+            score += 1
         else:
-            message = 'Incorrect!'
+            message = explain(question, user_answer)
+            score = 0
+            # message = 'Incorrect!'
 
         if score > high_score:  # Update the high score if necessary
             high_score = score
 
         # message = apiQ(user_answer)
 
+    question = apiQ("generate a hard question about politics and conflicts? Be specific. Make it short, less than 12 words Ex. Should we ban books that include hate speech? How do we solve the isreali palestinian conflict? How can governments address income inequality? How do we stop school shootings in America?")
 
-    return render_template('index.html', score=score, message=message, high_score=high_score)
+    return render_template('index.html', score=score, message=message, high_score=high_score, question=question)
 
 
 
@@ -46,16 +48,23 @@ def apiQ(content):
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": content}],
-        max_tokens=200,
-        temperature = 0
+        max_tokens=150,
+        temperature = 1
     )
     return(completion['choices'][0]['message']['content'])
 
 
 def decide(question, answer):
-    content = "Question: " + question + " Answer: " + answer + " Prompt: Is this is a good answer to the question? Answer Yes or No."
+    content = "Question: " + question + " Answer: " + answer + ". Prompt: Is this is a good answer to the question? Answer Yes or No."
 
-    v = apiQ(content)
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": content}],
+        max_tokens=150,
+        temperature = 0
+    )
+
+    v = completion['choices'][0]['message']['content']
 
     #see if yes is in v
     if "Yes" in v:
@@ -63,6 +72,10 @@ def decide(question, answer):
 
     return False
 
+def explain(question, answer):
+    content = "Question: " + question + " Answer: " + answer + ". Explain why this is a bad answer."
+    
+    return(apiQ(content))
 
 
 if __name__ == '__main__':
