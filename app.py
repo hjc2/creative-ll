75090781ea4656707abb8401ccfbe71ed99be760
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2023-05-02 16:30:13 -0500

    works,
    need to add dynamic questions

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from operator import contains
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -5,8 +6,6 @@ app = Flask(__name__)
 high_score = 0  # Initialize the high score to 0
 
 
-
-import os
 import openai
 
 import OPEN_API_KEY
@@ -21,10 +20,13 @@ def index():
 
     if request.method == 'POST':
         question = request.form['question']
-        expected_answer = request.form['expected_answer']
         user_answer = request.form['user_answer']
 
-        if user_answer.lower() == expected_answer.lower():
+        question = "How do we solve the isreal palestine conflict?"
+
+
+
+        if decide(question, user_answer):
             message = 'Correct!'
             score = 1
         else:
@@ -33,7 +35,7 @@ def index():
         if score > high_score:  # Update the high score if necessary
             high_score = score
 
-        message = apiQ(user_answer)
+        # message = apiQ(user_answer)
 
 
     return render_template('index.html', score=score, message=message, high_score=high_score)
@@ -48,6 +50,20 @@ def apiQ(content):
         temperature = 0
     )
     return(completion['choices'][0]['message']['content'])
+
+
+def decide(question, answer):
+    content = "Question: " + question + " Answer: " + answer + " Prompt: Is this is a good answer to the question? Answer Yes or No."
+
+    v = apiQ(content)
+
+    #see if yes is in v
+    if "Yes" in v:
+        return True
+
+    return False
+
+
 
 if __name__ == '__main__':
     app.run()
