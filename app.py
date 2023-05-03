6a2adb98a8b0a8dcd371c29c7a4e4cdd79edd8f6
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2023-05-03 14:50:59 -0500

    added some comments

--- app.py
+++ app.py
@@ -1,54 +1,59 @@
-from email import message
+
+
+# importing all the libraries
+
 from operator import contains
 from flask import Flask, render_template, request
 
 
 import values
 
+import openai
+
+import OPEN_API_KEY # we need the API key and its hidden in a seperate local file
+
 app = Flask(__name__)
 
-high_score = 0  # Initialize the high score to 0
+#Score Values for tracking gameplay
+high_score = 0
 score = 0
 
 
-import openai
-
-import OPEN_API_KEY
-
+# setting up the API key to be ready for use
 openai.api_key = OPEN_API_KEY.mykey
 
 
+
+# main page for the website
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global high_score  # Use the global variable
     global score
     message = ''
 
-    if request.method == 'POST':
-        user_answer = request.form['user_answer']
-        question = request.form['question']
+    if request.method == 'POST': # If the user has submitted an answer, check if it is correct
+        user_answer = request.form['user_answer'] # get the answer from the user
+        question = request.form['question'] # get the question from the page
 
 
-        if decide(question, user_answer):
+        if decide(question, user_answer): # if the answer is correct, it will say so, and continue the game
             message = 'Correct!'
             score += 1
 
         else:
-            message = explain(question, user_answer)
+            message = explain(question, user_answer) # if the answer is incorrect, it will explain why
             score = 0
-            # message = 'Incorrect!'
 
         if score > high_score:  # Update the high score if necessary
             high_score = score
 
-        # message = apiQ(user_answer)
-
+    # generates the next question
     question = apiQ(values.qgen)
 
-    return render_template('index.html', score=score, message=message, high_score=high_score, question=question)
+    return render_template('index.html', score=score, message=message, high_score=high_score, question=question) # updates the page with the new question and score
 
 
-
+# generates a question based on the question prompt
 def apiQ(content):
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -58,6 +63,9 @@ def apiQ(content):
     )
     return(completion['choices'][0]['message']['content'])
 
+
+
+# decides if an answer is correct or not
 
 def decide(question, answer):
     content = values.decideGen(question, answer)
@@ -91,6 +99,9 @@ def decide(question, answer):
 
     return False
 
+
+# writes the explanation as to why an answer is incorrect
+
 def explain(question, answer):
 
     content = values.explainGen(question, answer)
@@ -104,6 +115,6 @@ def explain(question, answer):
     
     return(completion['choices'][0]['message']['content'])
 
-
+# runs the app
 if __name__ == '__main__':
     app.run()
--- values.py
+++ values.py
@@ -1,15 +1,17 @@
 
 
-
+# prompt for generating a question.
 qgen = "generate a hard question about politics and conflicts? Be specific. Make it short, less than 12 words Ex. Should we ban books that include hate speech? How do we solve the isreali palestinian conflict? How can governments address income inequality? How do we stop school shootings in America?"
 
+# assembles the prompt for decisions
 def decideGen(question, answer):
     return("Question: " + question + " Answer: " + answer + ". Prompt: Is this is a good answer to the question? Answer Yes or No.")
 
-
+# assembles the prompt for explanations
 def explainGen(question, answer):
     # return("Question: " + question + " Answer: " + answer + ". Respond as a professor who is experienced in foreign policy and political science. Explain why this is an incorrect answer.")
     return("Question: " + question + " Answer: " + answer + ". Respond as a professor who is experienced in foreign policy and political science. Explain why this is an incorrect answer. Do not say who you are")
 
+# assembles the prompt for yes or no # backup for if No
 def yesNo(answer):
     return(answer + "does this answer say more Yes or No? Answer Yes or No.")
